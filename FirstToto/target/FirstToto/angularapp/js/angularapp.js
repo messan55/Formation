80bc3d2edc373bf6d2b4dd829e05a48f14ee6c34
Stagiaire <Stagiaire@PM8-PC4>
2016-01-13 15:43:15 +0100

    ng-module binding

--- FirstToto/target/FirstToto/angularapp/js/angularapp.js
+++ FirstToto/target/FirstToto/angularapp/js/angularapp.js
@@ -9,4 +9,29 @@ var todoApp = angular.module("todoApp", []);
 // le scope est automatiquement injecté par angular
 todoApp.controller("ToDoCtrl", function($scope){
     $scope.message = "Bonjour depuis toDOCtrl";
+    $scope.taches =[{   "category":"maison",
+                        "completed":false,
+                        "dateCreated":"2016-01-13T00:00:00",
+                        "id":1,"libelle":"passer aspirateur"
+                    },
+                    {   "category":"chambre",
+                        "completed":false,
+                        "dateCreated":"2016-01-13T00:00:00",
+                        "id":1,"libelle":"passer aspirateur"
+                    },
+                    {   "category":"maison",
+                        "completed":true,
+                        "dateCreated":"2016-01-13T00:00:00",
+                        "id":1,"libelle":"passer aspirateur"
+                    },
+                    {   "category":"salon",
+                        "completed":false,
+                        "dateCreated":"2016-01-13T00:00:00",
+                        "id":1,"libelle":"passer aspirateur"
+                    },
+                    {   "category":"terasse",
+                        "completed":true,
+                        "dateCreated":"2016-01-12T00:00:00",
+                        "id":2,"libelle":"passer serpière"
+                    }];
 });

62c49f8e74550e4d18d86785c725316de585aaeb
Stagiaire <Stagiaire@PM8-PC4>
2016-01-14 11:47:57 +0100

    requete ajax angular

--- FirstToto/src/main/webapp/angularapp/js/angularapp.js
+++ FirstToto/src/main/webapp/angularapp/js/angularapp.js
@@ -7,31 +7,82 @@ var todoApp = angular.module("todoApp", []);
 
 // creation d'un controlleur "todoctrl" dans notre module
 // le scope est automatiquement injecté par angular
-todoApp.controller("ToDoCtrl", function($scope){
+todoApp.controller("ToDoCtrl", function($scope, $http){
     $scope.message = "Bonjour depuis toDOCtrl";
-    $scope.taches =[{   "category":"maison",
+    $scope.taches = [];
+    $http.get('../rest/tache').then(function(response){
+        $scope.taches = response.data.taches;
+    });
+    /*$scope.taches =[{   "category":"maison",
                         "completed":false,
                         "dateCreated":"2016-01-13T00:00:00",
-                        "id":1,"libelle":"passer aspirateur"
+                        "id":1,
+                        "libelle":"passer aspirateur"
                     },
                     {   "category":"chambre",
                         "completed":false,
                         "dateCreated":"2016-01-13T00:00:00",
-                        "id":1,"libelle":"passer aspirateur"
+                        "id":1,
+                        "libelle":"passer aspirateur"
                     },
                     {   "category":"maison",
                         "completed":true,
                         "dateCreated":"2016-01-13T00:00:00",
-                        "id":1,"libelle":"passer aspirateur"
+                        "id":1,
+                        "libelle":"passer aspirateur"
                     },
                     {   "category":"salon",
                         "completed":false,
                         "dateCreated":"2016-01-13T00:00:00",
-                        "id":1,"libelle":"passer aspirateur"
+                        "id":1,
+                        "libelle":"passer aspirateur"
                     },
                     {   "category":"terasse",
                         "completed":true,
                         "dateCreated":"2016-01-12T00:00:00",
-                        "id":2,"libelle":"passer serpière"
-                    }];
+                        "id":2,
+                        "libelle":"passer serpière"
+                    }];*/
+    $scope.incompleteCount = function() {
+        var count = 0;
+        angular.forEach($scope.taches, function(t){
+           if(!t.completed) {
+               count++;
+           } 
+        });
+        return count;
+    };
+    
+    $scope.warningLevel = function() {
+        if($scope.incompleteCount() < 3){
+            return "label-success";
+        }
+        else{
+            return "label-warning";
+        }
+    };
+    
+    $scope.addNewTask = function(libelle, category){
+        var tid = $scope.taches.length + 1;
+        var tdate = "" + new Date();
+        $scope.taches.push({
+            "libelle" : libelle,
+            "category" : category,
+            "completed" : false,
+            "id" : tid,
+            "dateCreated" : tdate
+        });
+    }
+});
+
+todoApp.filter("completedTaskd", function(){
+    return function(items, showCompleted) {
+        var resultItems = [];
+        angular.forEach(items, function(item){
+            if (item.completed == false || showCompleted == true) {
+                resultItems.push(item);
+            }      
+        });
+        return resultItems;
+    };
 });
